67f66f9dbf1c3a72aa4b983d5b0072d46d3f7d7b
KaikyScript <kaikyscript.0day@gmail.com>
2022-08-22 21:19:15 -0300

    first commit - upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+    int main () 
+    {
+      
+    int num1, num2, soma;
+      
+    printf ("Digite um valor: ");
+      
+    scanf ("%d", &num1);
+      
+    printf ("Digite outro valor: ");
+      
+    scanf ("%d", &num2);
+      
+    soma = num1 + num2;
+      
+    printf ("O Resultado é: %d \n", soma);
+
+}
